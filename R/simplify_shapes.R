c6e37ad8b9ecc37b6de0a6cf167f7be2f3a426ba
Pedro R. Andrade <pedro.andrade@inpe.br>
2022-03-05 13:00:02 +0000

    version 2.0-1

--- R/simplify_shapes.R
+++ R/simplify_shapes.R
@@ -5,7 +5,7 @@
 #' reduce its size. It uses Douglas-Peucker algorithm internally.
 #'
 #' @param gtfs_data A list of data.tables read using gtfs2gps::read_gtfs().
-#' @param tol Numerical tolerance value to be used by the Douglas-Peuker algorithm.
+#' @param tol Numerical tolerance value to be used by the Douglas-Peucker algorithm.
 #' The default value is 0, which means that no data will be lost.
 #' @return A GTFS data whose shapes is a subset of the input data.
 #' @export
@@ -14,32 +14,26 @@
 #'
 #' poa_simpl <- simplify_shapes(poa)
 simplify_shapes <- function(gtfs_data, tol = 0){
+  
   gtfs_sf <- gtfs_shapes_as_sf(gtfs_data)
+  gtfs_sf$ID <- 1:nrow(gtfs_sf)
   
-  gtfs_st <- sf::as_Spatial(gtfs_sf)
-
-  gtfs_st_simpl <- rgeos::gSimplify(gtfs_st, tol)
-
-  # try to do something better
-  IDs <- gtfs_sf$shape_id
+  # geometry operations
+  gtfs_st_simpl <- terra::vect(gtfs_sf) %>% 
+    terra::simplifyGeom(x = .,tolerance = tol) %>% 
+    terra::geom() %>% 
+    data.table::as.data.table()
   
-  dt <- data.table::data.table()
+  # organize ouput data.table
+  gtfs_st_simpl[data.table::setDT(gtfs_sf)
+                , on = c("geom" = "ID")
+                ,shape_id := i.shape_id]
+  data.table::setnames(gtfs_st_simpl
+                       ,old = c("x","y")
+                       ,new = c("shape_pt_lon","shape_pt_lat"))
+  gtfs_st_simpl[,shape_pt_sequence := 1:.N,by = shape_id]
   
-  for(i in seq_along(IDs)){
-    coords <- gtfs_st_simpl@lines[[i]]@Lines[[1]]@coords
-    
-    df <- data.table::data.table(
-      shape_id = IDs[i],
-      shape_pt_lat = coords[, "y"],
-      shape_pt_lon = coords[, "x"],
-      shape_pt_sequence = 1:(dim(coords)[1])
-    )
-    
-    dt <- rbind(dt, df)
-    
-  }
-
-  gtfs_data$shapes <- dt
+  gtfs_data$shapes <- gtfs_st_simpl[,.(shape_id,shape_pt_lon,shape_pt_lat,shape_pt_sequence)]
   
   return(gtfs_data)
 }
